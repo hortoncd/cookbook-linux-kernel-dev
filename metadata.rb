0a8dfa2a01d5744628040d1ad10648bde4a1bd18
Chris Horton <hortoncd@gmail.com>
2016-08-27 08:53:01 -0700

    add issues and source urls to metadata and remove restrictions from circle.yml

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,8 @@ maintainer_email 'hortoncd@gmail.com'
 license          'Apache 2.0'
 description      'Installs/Configures assorted packages required for linux kernel development'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+source_url       'https://github.com/hortoncd/cookbook-linux-kernel-dev'
+issues_url       'https://github.com/hortoncd/cookbook-linux-kernel-dev/issues'
 version          '0.1.0'
 
 supports 'debian'
